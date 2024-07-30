40d1f0656a7effea8f083562db6a89a20411e720
bxxodev <bxxodev@gmail.com>
2024-07-31 00:19:30 +0200

    Update setup.bat

--- Discord-Rat/setup.bat
+++ Discord-Rat/setup.bat
@@ -1,6 +1,7 @@
 @echo off
-python -m venv venv
-call venv\Scripts\activate
-pip install -r requirements.txt
-echo All necessary packages have been installed.
+echo Installing required packages...
+pip install pyinstaller
+pip install discord pyttsx3 pyautogui requests opencv-python playsound pyaudio numpy pydub getpass4 Pillow pyftpdlib
+
+echo All required packages have been installed.
 pause
